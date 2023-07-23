2464469d00944240fe658813f69bf165ccab0e5c
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-07-23 21:57:11 +0300

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,11 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/lesson/:path*", "/settings/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/lesson/:path*",
+    "/settings/:path*",
+    "/explore/:path*",
+    "/play/:path*",
+  ],
 };
